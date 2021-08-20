a0121d3c09a3cddda3cc610badf9013befdfb72a
felix <kangkang.li@outlook.com>
2021-08-20 10:28:08 +0800

    main_felix::1.添加二叉树中层遍历;

---
+++ 29.IsSymmetric/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace _29.IsSymmetric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                对称二叉树
+                给定一个二叉树，检查它是否是镜像对称的。
+                 
+                
+                例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+                
+                    1
+                   / \
+                  2   2
+                 / \ / \
+                3  4 4  3
+                 
+                
+                但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+                
+                    1
+                   / \
+                  2   2
+                   \   \
+                   3    3
+                 
+                
+                进阶：
+                
+                你可以运用递归和迭代两种方法解决这个问题吗？
+                
+                相关标签
+                树
+                深度优先搜索
+                广度优先搜索
+                二叉树
+                
+                作者：力扣 (LeetCode)
+                链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xn7ihv/
+                来源：力扣（LeetCode）
+                著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+             */
+        }
+
+        public static bool IsSymmetric(TreeNode root)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+            return isSymmetric(root.left, root.right);
+        }
+
+        public static bool isSymmetric(TreeNode left, TreeNode right)
+        {
+            if (left == null && right == null)
+            {
+                return true;
+            }
+            if (left == null || right == null || left.val != right.val)
+            {
+                return false;
+            }
+            return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+        }
+
+    }
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ 30.LevelOrder/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _30.LevelOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                二叉树的层序遍历
+                给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。 
+                
+                示例：
+                二叉树：[3,9,20,null,null,15,7],
+                
+                    3
+                   / \
+                  9  20
+                    /  \
+                   15   7
+                返回其层序遍历结果：
+                
+                [
+                  [3],
+                  [9,20],
+                  [15,7]
+                ]
+                相关标签
+                树
+                广度优先搜索
+                二叉树
+                
+                作者：力扣 (LeetCode)
+                链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnldjj/
+                来源：力扣（LeetCode）
+                著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+             */
+        }
+        public static IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            IList<IList<int>> r = new List<IList<int>>();
+            if (root == null)
+            {
+                return r;
+            }
+            Queue<TreeNode> q = new Queue<TreeNode>();
+            q.Enqueue(root);
+
+            while (q.Count > 0)
+            {
+                // 一个队列做临时存储区域,用一个len变量做长度记录,这样每次for循环的时候就会把上次的存储的元素移除;
+                List<int> curIndex = new List<int>();
+                int len = q.Count;
+                for (int i = 0; i < len; i++)
+                {
+                    TreeNode temp = q.Dequeue();
+                    curIndex.Add(temp.val);
+                    if (temp.left != null)
+                        q.Enqueue(temp.left);
+                    if (temp.right != null)
+                        q.Enqueue(temp.right);
+                }
+                r.Add(curIndex);
+            }
+            return r;
+        }
+    }
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
