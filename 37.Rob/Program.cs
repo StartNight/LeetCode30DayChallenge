f471af0382211af9eceb73444f6323587801cabc
felix <kangkang.li@outlook.com>
2021-08-30 17:29:18 +0800

    main_felix::1.添加37.打家劫舍.38.打乱数组.39.最小栈;
    Signed-off-by: felix <kangkang.li@outlook.com>

---
+++ 37.Rob/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace _37.Rob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                打家劫舍
+                你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+                
+                给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+                
+                 
+                
+                示例 1：
+                
+                输入：[1,2,3,1]
+                输出：4
+                解释：偷窃 1 号房屋 (金额 = 1) ，然后偷窃 3 号房屋 (金额 = 3)。
+                     偷窃到的最高金额 = 1 + 3 = 4 。
+                示例 2：
+                
+                输入：[2,7,9,3,1]
+                输出：12
+                解释：偷窃 1 号房屋 (金额 = 2), 偷窃 3 号房屋 (金额 = 9)，接着偷窃 5 号房屋 (金额 = 1)。
+                     偷窃到的最高金额 = 2 + 9 + 1 = 12 。
+                 
+                
+                提示：
+                
+                1 <= nums.length <= 100
+                0 <= nums[i] <= 400
+                相关标签
+                数组
+                动态规划
+                
+                作者：力扣 (LeetCode)
+                链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnq4km/
+                来源：力扣（LeetCode）
+                著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+             */
+        }
+
+        /*
+            核心思想是，到了第i个房间时，要决定偷还是不偷
+            偷的话 就是 nums[i] + dp[i-2]
+            不偷的话就是 dp[i-1]
+            取两者最大收益即可
+         */
+        public int Rob(int[] nums)
+        {
+            if (nums.Length <= 0)
+            {
+                return 0;
+            }
+            if (nums.Length == 1)
+            {
+                return nums[0];
+            }
+            if (nums.Length == 2)
+            {
+                return Math.Max(nums[0], nums[1]);
+            }
+            int[] dp = new int[nums.Length];
+            int max = dp[0];
+            dp[0] = nums[0];
+            dp[1] = Math.Max(nums[0], nums[1]);
+            for (int i = 2; i < nums.Length; i++)
+            {
+                dp[i] = Math.Max(dp[i - 1], nums[i] + dp[i - 2]);
+            }
+            for (int i = 0; i < dp.Length; i++)
+            {
+                if (dp[i] > max)
+                {
+                    max = dp[i];
+                }
+            }
+            return max;
+
+        }
+    }
+}
---
+++ 39.MinStack/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace _39.MinStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                最小栈
+                设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+                
+                push(x) —— 将元素 x 推入栈中。
+                pop() —— 删除栈顶的元素。
+                top() —— 获取栈顶元素。
+                getMin() —— 检索栈中的最小元素。
+                 
+                
+                示例:
+                
+                输入：
+                ["MinStack","push","push","push","getMin","pop","top","getMin"]
+                [[],[-2],[0],[-3],[],[],[],[]]
+                
+                输出：
+                [null,null,null,null,-3,null,0,-2]
+                
+                解释：
+                MinStack minStack = new MinStack();
+                minStack.push(-2);
+                minStack.push(0);
+                minStack.push(-3);
+                minStack.getMin();   --> 返回 -3.
+                minStack.pop();
+                minStack.top();      --> 返回 0.
+                minStack.getMin();   --> 返回 -2.
+                 
+                
+                提示：
+                
+                pop、top 和 getMin 操作总是在 非空栈 上调用。
+                
+                作者：力扣 (LeetCode)
+                链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnkq37/
+                来源：力扣（LeetCode）
+                著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+             */
+        }
+    }
+
+    public class MinStack
+    {
+        //链表头，相当于栈顶
+        private ListNode head;
+
+        /** initialize your data structure here. */
+        public MinStack()
+        {
+        }
+
+        //压栈，需要判断栈是否为空
+        public void Push(int x)
+        {
+            if (Empty())
+                head = new ListNode(x, x, null);
+            else
+                head = new ListNode(x, Math.Min(x, head.min), head);
+        }
+
+        //出栈，相当于把链表头删除
+        public void Pop()
+        {
+            if (Empty())
+                throw new Exception("栈为空……");
+            head = head.next;
+        }
+
+        //栈顶的值也就是链表头的值
+        public int Top()
+        {
+            if (Empty())
+                throw new Exception("栈为空……");
+            return head.val;
+        }
+
+        //链表中头结点保存的是整个链表最小的值，所以返回head.min也就是
+        //相当于返回栈中最小的值
+        public int GetMin()
+        {
+            if (Empty())
+                throw new Exception("栈为空……");
+            return head.min;
+        }
+        //判断栈是否为空
+        private bool Empty()
+        {
+            return head == null;
+        }
+    }
+    class ListNode
+    {
+        public int val;
+        public int min;//最小值
+        public ListNode next;
+
+        public ListNode(int val, int min, ListNode next)
+        {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+
+}
